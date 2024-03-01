726f73c4bd15b6f34ab840748776243d43370b20
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2024-03-01 20:54:31 -0300

    Remove not finished video-game-cli

--- cmd/video-game-cli/run.go
+++ cmd/video-game-cli/run.go
@@ -1,12 +1,1 @@
 package main
-
-// `import (
-// 	"log/slog"
-
-// 	"github.com/spf13/cobra"
-// 	"github.com/spf13/viper"
-// )
-
-// func Run(cmd *cobra.Command, v *viper.Viper, logger *slog.Logger) {
-
-// }
